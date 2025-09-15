7d1f65e7d8030af04afdf127fcdeb37bb4b70939
mimifuwa <mimifuwa.cc@gmail.com>
2025-09-15 17:16:15 +0900

    build: vercelではopennextの設定を適用しないように変更

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,14 @@ const nextConfig: NextConfig = {
 
 export default nextConfig;
 
-import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
-
-initOpenNextCloudflareForDev();
+// don't load opennext config on Vercel
+if (process.env.VERCEL !== "1") {
+  try {
+    const { initOpenNextCloudflareForDev } = await import(
+      "@opennextjs/cloudflare"
+    );
+    initOpenNextCloudflareForDev();
+  } catch (_error) {
+    console.info("Cloudflare integration skipped");
+  }
+}
