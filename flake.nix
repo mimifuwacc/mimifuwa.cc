e6a9c0ee0c09787b0e7627bcafff1a7bc493bae6
mimifuwacc <mail@mimifuwa.cc>
2026-03-23 01:04:00 +0900

    chore: 開発環境をnixに

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "mimifuwacc/mimifuwa.cc";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          nodejs_24
+          pnpm_10
+        ];
+      };
+    });
+}
